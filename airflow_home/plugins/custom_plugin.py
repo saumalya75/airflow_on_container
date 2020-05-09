566ef81df06b29e254b90e42cc6d383734e298d6
Saumalya Sarkar <potter@Saumalyas-MacBook-Air.local>
2020-05-10 02:39:12 +0530

    Due to some issues, step by step commits are lost. Commiting all changes together. Changes: 	1. Airflow version is bumped upto 1.10.9, 	2. custom_plugin, custom_sensor, custom_hook and custom_operator is introduced in the demo dag, 	3. Trimmed down the puckel_ repo to fit only this application's need, 	4. puckel_ repo directory structures are modified, 	5. XCOM utility is introduced, 	6. Ready to be launched in Local/Sequential/Celery mode, two separate docker-compose files are provided.

---
+++ airflow_home/plugins/custom_plugin.py
@@ -0,0 +1,10 @@
+from airflow.plugins_manager import AirflowPlugin
+from sensors.custom_s3_sensor import CustomS3Sensor
+from operators.custom_file_load_operator import CustomFileProcessingOperator
+
+
+# Defining the plugin class
+class CustomPlugin(AirflowPlugin):
+    name = "custom_plugin"
+    sensors = [CustomS3Sensor]
+    operators = [CustomFileProcessingOperator]
---
+++ airflow_home/plugins/operators/custom_file_load_operator.py
@@ -0,0 +1,62 @@
+from airflow.models.baseoperator import BaseOperator
+from hooks.custom_s3_minio_hook import CustomS3MinioHook
+from airflow.utils.decorators import apply_defaults
+
+
+class CustomFileProcessingOperator(BaseOperator):
+    """
+        Currently this operator is not doing anything practically.
+        We are just taking count of the source file and putting it on XCOM or demo purpose.
+        But anyone can add logic to based on hteir reqirement. The configurations are structure will be similar.
+    """
+
+    @apply_defaults
+    def __init__(self
+        , xcom_task_id_key:str = 'data_processor_key'
+        , xcom_source_task_id:str = ''
+        , xcom_key:str = ''
+        , source_bed_type:str = 'aws'
+        , source_endpoint_url:str = ''
+        , source_conn_id:str = 'aws_default'
+        , source_verify:str = ''
+        , *args
+        , **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        if (not len(xcom_key)) or (not len(xcom_source_task_id)):
+            raise AirflowException("Providing XCOM source task name and key is mandatory for for this processing operator.")
+        self.xcom_task_id_key = xcom_task_id_key
+        self.xcom_task_id = xcom_source_task_id
+        self.xcom_key = xcom_key
+        self.source_bed_type = source_bed_type
+        self.source_endpoint_url=source_endpoint_url
+        self.source_conn_id=source_conn_id
+        self.source_verify=source_verify
+
+    @staticmethod
+    def _extract_xcom_data(task_instance, xcom_task_id, xcom_key):
+        return task_instance.xcom_pull(xcom_task_id, key=xcom_key)
+
+    def execute(self, context):
+        """File processing is implemented"""
+        source_hook = CustomS3MinioHook(
+            conn_type=self.source_bed_type
+            , endpoint_url=self.source_endpoint_url
+            , aws_conn_id=self.source_conn_id
+            , verify=self.source_verify
+        )
+        print(f"Source Connection Type: {self.source_bed_type}")
+
+        task_instance = context['task_instance']
+        self.source_bucket_name = self._extract_xcom_data(task_instance, self.xcom_task_id, self.xcom_key + "__bucket")
+        self.source_key = self._extract_xcom_data(task_instance, self.xcom_task_id, self.xcom_key + "__prefix") \
+            + '/' \
+            + self._extract_xcom_data(task_instance, self.xcom_task_id, self.xcom_key + "__key")
+        source_data = source_hook.read_key(self.source_key, self.source_bucket_name)
+        print(source_data)
+        task_instance.xcom_push(
+            key=self.xcom_task_id_key + '__row_count'
+            , value = len(source_data.splitlines())
+        )
+
+        print("Execution complete!")
